157a66ecc7d7402c383b8f1234346b470bef7bdb
kozec <kozec@kozec.com>
2014-08-14 09:18:34 +0200

    Fix: SIGINT getting lost somewhere between GLib and python

--- syncthing-gtk.py
+++ syncthing-gtk.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python2
-import sys
+import sys, signal
+
+def sigint(*a):
+	print("\n*break*")
+	sys.exit(0)
 
 if __name__ == "__main__":
 	from syncthing_gtk import App
+	signal.signal(signal.SIGINT, sigint)
 	App("-w" not in sys.argv, ".", "./icons").run()
