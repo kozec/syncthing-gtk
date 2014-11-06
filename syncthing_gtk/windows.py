1d8c3f8bb76064d621b55281c513267dd724b8f9
kozec <kozec@kozec.com>
2014-11-06 13:25:26 +0100

    Reworked DaemonProcess implementation on Windows; Hides console window

--- syncthing_gtk/windows.py
+++ syncthing_gtk/windows.py
@@ -8,7 +8,8 @@ from syncthing_gtk import windows
 
 from __future__ import unicode_literals
 from syncthing_gtk.tools import IS_WINDOWS
-import codecs
+from gi.repository import Gio, GLib, GObject
+import os, codecs, msvcrt, win32pipe
 
 def fix_localized_system_error_messages():
 	"""
@@ -28,3 +29,65 @@ def fix_localized_system_error_messages():
 		return (u'?', error.end)
 	
 	codecs.register_error("strict", handle_error)
+
+class WinPopenReader:
+	"""
+	Reads from PIPE using GLib timers or idle_add. Emulates part of
+	UnixInputStream, but its in no way even close to complete
+	emulation.
+	
+	This is only way that I found so far to have pipe and hidden
+	console window on Windows.
+	"""
+	
+	def __init__(self, process):
+		# Prepare stuff
+		self._process = process
+		self._waits_for_read = None
+		self._buffer = ""
+		self._buffer_size = 32
+		self._closed = False
+		self._stdouthandle = msvcrt.get_osfhandle(self._process.stdout.fileno())
+		# Start reading
+		GLib.idle_add(self._peek)
+	
+	def _peek(self):
+		if self._closed:
+			return False
+		# Check if there is anything to read and read if available
+		(read, nAvail, nMessage) = win32pipe.PeekNamedPipe(self._stdouthandle, 0)
+		if nAvail >= self._buffer_size:
+			data = self._process.stdout.read(self._buffer_size)
+			self._buffer += data
+		# If there is read_async callback and buffer has enought of data,
+		# send them right away
+		if not self._waits_for_read is None and len(self._buffer) > self._buffer_size:
+			r = WinPopenReader.Results(self._buffer[0:self._buffer_size])
+			self._buffer = self._buffer[self._buffer_size:]
+			callback, data = self._waits_for_read
+			self._waits_for_read = None
+			callback(self, r, *data)
+			GLib.idle_add(self._peek)
+			return False
+		GLib.timeout_add_seconds(1, self._peek)
+		return False
+	
+	def read_bytes_async(self, size, trash, cancel, callback, data=()):
+		if self._waits_for_read != None:
+			raise Exception("Already reading")
+		self._buffer_size = size
+		self._waits_for_read = (callback, data)
+	
+	def read_bytes_finish(self, results):
+		return results
+	
+	def close(self):
+		self._closed = True
+	
+	class Results:
+		""" Also serves as response object """
+		def __init__(self, data):
+			self._data = data
+		
+		def get_data(self):
+			return self._data
