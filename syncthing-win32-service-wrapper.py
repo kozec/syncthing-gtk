402095dae2718381bf715ddc93deee1b3f46b34f
kozec <trash at kozec dot com>
2014-11-11 22:23:04 +0100

    Service in python works

--- syncthing-win32-service-wrapper.py
+++ syncthing-win32-service-wrapper.py
@@ -1,22 +1,70 @@
-from gi.repository import GObject
-import win32serviceutil
-import win32service
-import win32event
-import win32evtlogutil
-import servicemanager
-import socket
-import time
-import logging
+DEBUG = True
 
-class SyncthingService (win32serviceutil.ServiceFramework):
+from gi.repository import GObject
+from syncthing_gtk import DaemonProcess, Configuration
+import win32serviceutil, win32service, win32event, servicemanager
+import socket, time, logging, sys
+
+class DaemonData:
+	def __init__(self):
+		self.process = None
+		self.stop_requested = False
+		self.config = Configuration()
+		self.mainloop = GObject.MainLoop()
+	
+	def cb_daemon_startup_failed(self, *a):
+		logging.error("Failed to start syncthing daemon")
+	
+	def cb_daemon_exit(self, daemon, exit_code):
+		if exit_code == 0:
+			logging.info("Daemon shat down")
+			self.stop_requested = True
+		else:
+			logging.info("Daemon exited with code %s. Restarting..." % (exit_code,))
+			self.start_daemon()
+	
+	def cb_daemon_line(self, daemon, line):
+		logging.info(line)
+	
+	def start_daemon(self):
+		logging.info('Starting daemon...')
+		self.process = DaemonProcess([self.config["syncthing_binary"], "-no-browser"])
+		self.process.connect('failed', self.cb_daemon_startup_failed)
+		self.process.connect('exit', self.cb_daemon_exit)
+		if DEBUG:
+			self.process.connect('line', self.cb_daemon_line)
+		self.process.start()
+	
+	def stop_daemon(self):
+		if not self.process is None:
+			logging.info('Killing daemon ...')
+			self.process.kill()
+			self.process = None
+	
+	def main(self):
+		logging.info('Starting %s ...' % (SyncthingService._svc_name_,))
+		self.start_daemon()
+		
+		# Simulate a main loop
+		while True:
+			if self.stop_requested:
+				logging.info('A stop signal was received: Breaking main loop ...')
+				break
+			self.mainloop.get_context().iteration(False)
+			time.sleep(1)
+		self.stop_daemon()
+		return
+	
+	
+class SyncthingService (win32serviceutil.ServiceFramework, DaemonData):
 	_svc_name_ = "Syncthing-Service"
 	_svc_display_name_ = "Syncthing Service"
 	
 	def __init__(self,args):
 		win32serviceutil.ServiceFramework.__init__(self,args)
+		DaemonData.__init__(self)
 		self.stop_event = win32event.CreateEvent(None,0,0,None)
 		socket.setdefaulttimeout(60)
-		self.stop_requested = False
 
 	def SvcStop(self):
 		self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
@@ -32,26 +80,21 @@ class SyncthingService (win32serviceutil.ServiceFramework):
 		)
 		self.main()
 
-	def main(self):
-		logging.info('Starting %s ...' % (SyncthingService._svc_name_,))
-		mainloop = GObject.MainLoop()
-		# Simulate a main loop
-		while True:
-			if self.stop_requested:
-				logging.info('A stop signal was received: Breaking main loop ...')
-				break
-			mainloop.get_context().iteration(False)
-			logging.info("Hello at %s" % time.ctime())
-			time.sleep(1)
-		return
-
-logging.basicConfig(
-	filename = 'c:\\Temp\\%s.log' % (SyncthingService._svc_name_,),
-	level = logging.DEBUG, 
-	format = '[%s] %%(levelname)-7.7s %%(message)s' % (SyncthingService._svc_name_,)
-)
+if DEBUG and not "test" in sys.argv:
+	logging.basicConfig(
+		filename = 'c:\\Temp\\%s.log' % (SyncthingService._svc_name_,),
+		level = logging.DEBUG, 
+		format = '[%s] %%(levelname)-7.7s %%(message)s' % (SyncthingService._svc_name_,)
+	)
+else:
+	logging.basicConfig(
+		file = sys.stdout,
+		level = logging.DEBUG, 
+		format = '[%s] %%(levelname)-7.7s %%(message)s' % (SyncthingService._svc_name_,)
+	)
 
 if __name__ == '__main__':
-	# win32serviceutil.HandleCommandLine(SyncthingService)
-	
-	
+	if "test" in sys.argv:
+		d = DaemonData().main()
+	else:
+		win32serviceutil.HandleCommandLine(SyncthingService)
