99ff11f2feb703e92382abd56f04a3d3b3a2b63f
Geoffrey Frogeye <geoffrey@frogeye.fr>
2014-12-02 21:49:39 +0100

    Added Chocolatey packaging utilities

---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'syncthing-gtk'
+$installerType = 'EXE'
+$url = 'https://github.com/syncthing/syncthing-gtk/releases/download/v0.5.2/SyncthingGTK-0.5.2-win32-full-installer.exe'
+$url64 = $url
+$silentArgs = '/S'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
