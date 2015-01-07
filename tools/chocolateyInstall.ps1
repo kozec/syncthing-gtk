9d81a121808fec60021d75e60621f653de0f0144
kozec <kozec@kozec.com>
2015-01-07 10:34:28 +0100

    Removed everything not related to 1st run wizard

--- tools/chocolateyInstall.ps1
+++
@@ -1,7 +0,0 @@
-$packageName = 'syncthing-gtk'
-$installerType = 'EXE'
-$url = 'https://github.com/syncthing/syncthing-gtk/releases/download/v0.5.2/SyncthingGTK-0.5.2-win32-full-installer.exe'
-$silentArgs = '/S'
-$validExitCodes = @(0)
-
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
