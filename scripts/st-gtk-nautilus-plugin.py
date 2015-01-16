c38750e20a002ad857fb0824c179c44f2967a317
kozec <kozec@kozec.com>
2015-01-16 01:21:56 +0100

    DBus plugin: Signal handling & ST-GTK start detection

--- scripts/st-gtk-nautilus-plugin.py
+++ scripts/st-gtk-nautilus-plugin.py
@@ -11,44 +11,44 @@ DEBUG	= True
 # ST-GTK service data
 SERVICE	= 'net.syncthing.syncthinggtk'
 PATH	= '/net/syncthing/SyncthingGTK'
+IFACE	= SERVICE
 
-class OnituIconOverlayExtension(GObject.GObject, Nautilus.InfoProvider):
+class STGTKExtension_Nautilus(GObject.GObject, Nautilus.InfoProvider):
 	def __init__(self):
+		# Prepare stuff
 		init_logging()
 		set_logging_level(VERBOSE, DEBUG)
 		log.info("Initializing...")
-		self.connection = Gio.bus_get_sync(Gio.BusType.SESSION, None)
+		self.stgtk = None
 		self.folders = []
-		# Try connect to ST-GTK instance. This will most likely fail,
-		# as ST-GTK is not first thing starting before nautilus, but
-		# it's necessary to prevent situation when plugin starts only
+		# Connect to DBus & ST-GTK instance
+		Gio.DBusProxy.new_for_bus(Gio.BusType.SESSION,
+			Gio.DBusProxyFlags.NONE, None, SERVICE, PATH, IFACE,
+			None, self._cb_got_interface)
+	
+	### Internal stuff
+	def _cb_got_interface(self, bus, res):
+		self.stgtk = Gio.DBusProxy.new_for_bus_finish(res)
+		self.stgtk.connect("g-signal", self._cb_g_signal)
+		# Try call ST-GTK instance. This will most likely fail,
+		# as ST-GTK is usualy not starting before nautilus, but it's
+		# necessary to prevent situation when plugin starts only
 		# after connected event is emited.
 		try:
-			reply = self._request("ping", '(s)')
-			connected, = self._request("is_connected", '(b)')
-			if connected:
-				log.info("ST-GTK running and connected to daemon")
-				self._read_folders()
+			if self.stgtk.is_connected():
+				self._cb_connected(self.stgtk)
 		except GLib.Error:
-			# Syncthing-GTK is not up yet
-			log.info("ST-GTK not running.")
+			pass
 	
-	def _request(self, method, answer_fmt, *arguments):
-		args = None
-		if len(arguments) > 0:
-			variants = []
-			for a in arguments:
-				if type(a) == str:
-					variants.append(GLib.Variant.new_string(a))
-				else:
-					raise TypeError("Cannot convert type %s" % type(a))
-			args = GLib.Variant.new_tuple(*variants)
-		reply = self.connection.call_sync(SERVICE, PATH,
-					SERVICE, method, args,
-					GLib.VariantType.new (answer_fmt),
-					Gio.DBusCallFlags.NONE,
-					-1, None)
-		return reply
+	def _cb_g_signal(self, src, no_idea, dbus_signal, pars):
+		"""
+		Handler for "recieved-dbus-signal" signal. Searchs for associated
+		dbus signal handler and calls it, if found.
+		"""
+		if hasattr(self, "_cb_%s" % (dbus_signal,)):
+			getattr(self, "_cb_%s" % (dbus_signal,))(src, *pars)
+		else:
+			print "Not handled:", dbus_signal, pars
 	
 	def _read_folders(self, *a):
 		"""
@@ -56,15 +56,17 @@ class OnituIconOverlayExtension(GObject.GObject, Nautilus.InfoProvider):
 		plugin needs to know this list before nautilus starts requesting
 		for file information.
 		"""
-		self.folders, = self._request("get_folders", "(as)")
+		self.folders = self.stgtk.get_folders()
 		log.debug("Read %s folders", len(self.folders))
 	
+	### Plugin stuff
 	def update_file_info(self, file):
 		# Check if folder is one of repositories managed by syncthing
 		path = file.get_location().get_path()
 		if path in self.folders:
 			# Determine what emblem should be used
-			state, = self._request("get_folder_state", "(s)", path)
+			state = self.stgtk.get_folder_state("(s)", path)
+			print path, state
 			if state in ("idle", "scanning"):
 				# File manager probably shoudn't care about folder being scanned
 				file.add_emblem("syncthing")
@@ -75,3 +77,11 @@ class OnituIconOverlayExtension(GObject.GObject, Nautilus.InfoProvider):
 			else:
 				# Default (i-have-no-idea-what-happened) state
 				file.add_emblem("syncthing-offline")
+	
+	### DBus signal handlers
+	def _cb_connected(self, src, *a):
+		""" Handler for 'connected' dbus signal """
+		self._read_folders()
+	
+	def _cb_folder_state_changed(self, *a):
+		print "_cb_folder_state_changed", a
