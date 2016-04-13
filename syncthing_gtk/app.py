0ac5d035b59002ed6d4382f9ac59903854d79e47
kozec <kozec2@kozec.com>
2016-04-13 10:54:26 +0200

    Bumped internal version

--- syncthing_gtk/app.py
+++ syncthing_gtk/app.py
@@ -15,7 +15,7 @@ import os, webbrowser, sys, time, logging, shutil, re
 log = logging.getLogger("App")
 
 # Internal version used by updater (if enabled)
-INTERNAL_VERSION		= "v0.8"
+INTERNAL_VERSION		= "v0.8.5"
 # Minimal Syncthing version supported by App
 MIN_ST_VERSION			= "0.13.0"
 
