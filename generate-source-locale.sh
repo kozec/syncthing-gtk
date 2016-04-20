c1cf3f2bdbdb4010c2b794a73672561f4cd44c63
kozec <kozec@kozec.com>
2016-04-20 11:30:06 +0200

    Updated source locale

--- generate-source-locale.sh
+++ generate-source-locale.sh
@@ -3,15 +3,16 @@
 # Constants
 GETTEXT_DOMAIN="syncthing-gtk"
 MESSAGES_PO="messages.po"
+LOCALEDIR="locale"
 SRCLOCALE="en"
 
 # Generate messages.po
 [ -e ${MESSAGES_PO} ] && rm ${MESSAGES_PO}
 xgettext -e syncthing_gtk/*.py *.glade
 
-filename=${SRCLOCALE}/LC_MESSAGES/${GETTEXT_DOMAIN}.po
-mo=${SRCLOCALE}/LC_MESSAGES/${GETTEXT_DOMAIN}.mo
-mkdir -p ${SRCLOCALE}/LC_MESSAGES
+filename=${LOCALEDIR}/${SRCLOCALE}/LC_MESSAGES/${GETTEXT_DOMAIN}.po
+mo=${LOCALEDIR}/${SRCLOCALE}/LC_MESSAGES/${GETTEXT_DOMAIN}.mo
+mkdir -p ${LOCALEDIR}/${SRCLOCALE}/LC_MESSAGES
 if [ -e ${filename} ] ; then
 	# Merge new strings to existing po file
 	msgmerge --update ${filename} ${MESSAGES_PO} || exit 1
